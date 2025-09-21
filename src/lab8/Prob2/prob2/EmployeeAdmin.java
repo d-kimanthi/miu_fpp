064995e329193f92440a9788b8ae442690b7d3ca
Kimanthi <kimanthi@Kimanthi-Mac.local>
2025-09-20 20:56:55 -0500

    add prepareReport

--- src/lab8/Prob2/prob2/EmployeeAdmin.java
+++ src/lab8/Prob2/prob2/EmployeeAdmin.java
@@ -1,5 +1,7 @@
 package lab8.Prob2.prob2;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -13,7 +15,18 @@ public class EmployeeAdmin {
 	*/
 	public static List<Employee> prepareReport(HashMap<String, Employee> table, List<String> socSecNums) {
 		//IMPLEMENT
-		return null;
+        List<Employee> result = new ArrayList<>();
+
+        for (String ssn : socSecNums) {
+            Employee employee = table.get(ssn);
+            if (employee != null && employee.getSalary() > 80000) {
+                result.add(employee);
+            }
+        }
+
+        Collections.sort(result, (e1, e2) -> e1.getSsn().compareTo(e2.getSsn()));
+
+        return result;
 		
 	}
 	
