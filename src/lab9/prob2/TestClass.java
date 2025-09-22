8b853d27164a43eaebecbb2b605762495733370e
fkvivid <vividtemka@gmail.com>
2025-09-22 12:00:51 -0500

    update

--- src/lab9/prob2/TestClass.java
+++ src/lab9/prob2/TestClass.java
@@ -5,26 +5,31 @@ public class TestClass {
         CustomerAccount ca1 = new CustomerAccount("cusName-1", "accNo-1",200);
 
         // Invalid requests
+        // invalid $0 deposit
         try {
             ca1.deposit(0);
         }catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
         }
+        // invalid minus $-10 deposit
         try {
             ca1.deposit(-10);
         }catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
         }
+        // invalid minus $-10 withdraw
         try {
             ca1.withdraw(-10);
         }catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
         }
+        // invalid account limit exceeded withdraw
         try {
             ca1.withdraw(2000);
         }catch (AccountException e){
             System.out.println(e.getMessage());
         }
+        // invalid withdraw account cannot be below $100
         try {
             ca1.withdraw(100);
         }catch (AccountException e){
